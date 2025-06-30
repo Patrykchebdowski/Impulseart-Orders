31224ec346aaa32cbc29165576999556490e5283
Patrykchebdowski <patrykchebdowski@gmail.com>
2025-06-30 18:03:33 +0100

    11

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,15 @@
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(cache => cache.addAll(['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png']))
+    caches.open('impulseart-orders-v1').then(function(cache) {
+      return cache.addAll(['/', '/index.html']);
+    })
   );
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App.jsx';
+
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js'));
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
 }
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
