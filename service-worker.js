36414ecbf0a8bd83b19df27ba4f54a3b405bc796
Patrykchebdowski <patrykchebdowski@gmail.com>
2025-06-30 17:16:02 +0100

    gh

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Installed");
+self.addEventListener('install', function(e) {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', function(e) {
   e.respondWith(fetch(e.request));
 });
